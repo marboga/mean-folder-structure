493efb53d669dcadf5286f5c060e3a34728acedc
Michael Arbogast <marboga@gmail.com>
2016-06-03 07:31:18 -0700

    Revert "a couple more touches."
    This reverts commit 2313fdc615d51e7a18f8b9ec6c51c39c80ca572d.
    
    fixed issue.

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-console.log('user model loaded');
-
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-	name: String,
-}, {timestamps: true})
-
-mongoose.model('User', UserSchema)
